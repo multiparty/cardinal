876385d41243dd7c3446be84c29723422b09c4d1
Ammar Ahmad <ammar.977@hotmail.com>
2021-06-14 13:09:32 -0400

    added endpoint for pod status

--- wsgi.py
+++ wsgi.py
@@ -230,6 +230,36 @@ def workflow_complete():
 
         return jsonify(response)
 
+@app.route("/api/status", methods=["POST"])
+def get_status():
+    """
+    Returns the status of the pod.
+    """
+
+    if request.method == "POST":
+        """
+        request looks like:
+        {
+            "workflow_name": "test-workflow"
+        }
+        """
+
+        req = request.get_json(force=True)
+        if req["workflow_name"] in RUNNING_JOBS:
+            status = RUNNING_JOBS[req['workflow_name']].get_pod_status()
+            response = {
+                "status": status
+            }
+        else:
+            app.logger.error(
+                f"Received request asking the pod status in {req['workflow_name']} "
+                f"but this workflow is not present in running jobs"
+                f"record. Nothing to do.")
+            response = {
+                "MSG": f"ERR: {req['workflow_name']} not in running jobs record."
+            }
+
+        return jsonify(response)
 
 if __name__ != "__main__":
 
@@ -241,4 +271,4 @@ if __name__ != "__main__":
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="127.0.0.1", port=port, debug=True)
